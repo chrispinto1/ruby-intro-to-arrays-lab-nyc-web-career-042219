396eaf779252d6614f39ffdefee58d611f7cd85d
Christian Pinto <pintochristian2@gmail.com>
2019-04-15 01:36:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -16,10 +16,10 @@ def third_element(array)
   return array[2]
 end
 
-def last_element(array
+def last_element(array)
   return array[-1]
 end
 
 def first_element_with_array_methods(array)
-  array.last  
+  return array.last  
 end
